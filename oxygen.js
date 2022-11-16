c24bb9b4c35f9ec70e21207cb573c7968d7d8003
Anton <as.ky@ya.ru>
2022-11-16 16:21:55 +0300

    maded some great changes

--- oxygen.js
+++ oxygen.js
@@ -6,8 +6,8 @@ var ox = (
         let indexes = [];
         let isInitMode = true;
         let helper = {
-            scanHTML: function(element, fn, inOrder = false){
-                if(typeof fn !== 'function'){
+            scanHTML: function(element, callback, inOrder = false){
+                if(typeof callback !== 'function'){
                     throw new Error('Fn parameter must be an function!');    
                 }
                 let arr = [{el:element, i:0}];
@@ -15,7 +15,7 @@ var ox = (
                     let item = arr.pop();
                     for(let i = item.i; i < item.el.childElementCount; i++){
                         try{
-                            fn(item.el.children[i]);
+                            callback(item.el.children[i]);
                         }
                         catch(ex){
                             if(ex.message === 'BCI')
@@ -38,15 +38,19 @@ var ox = (
                 }
             },
             
-            scanBinder: function(el, cntx = context){
+            scanBinderMX: function(el){
                 let item = el;
                 for (let j = 0; j < item.attributes.length; j++){
                     for (let l = 0; l < listBindings.length; l++){
                         if(item.attributes[j].name === listBindings[l].binder){
-                            with(cntx){result = eval("(" + item.attributes[j].value + ")");};
-                            elements.push({element:item, binding:item.attributes[j].name, listIndexes:indexes, expression:item.attributes[j].value, context:cntx, index:cntx.$index});  
-                            indexes = [];     
-                            listBindings[l].fn(item, result);                           
+                            let result;
+                            with(this){result = listBindings[l].calc ? eval("(" + item.attributes[j].value + ")") : item.attributes[j].value};
+                            if(indexes.length > 0)
+                            {
+                                elements.push({element:item, binding:item.attributes[j].name, listIndexes:indexes, expression:item.attributes[j].value, context:this, index:this.$index});  
+                                indexes = [];   
+                            }
+                            listBindings[l].fn.call(this, item, result);                           
                         }
                     }
                 }
@@ -59,7 +63,13 @@ var ox = (
                     }
                 }
             },
-
+            findElementIndex: function(el){
+                for(let i = 0; i < elements.length; i++){
+                    if(el === elements[i].element){
+                        return i;
+                    }
+                }
+            },
             findElementDESC: function(el){
                 for(let i = elements.length - 1; i > -1; i--){
                     if(el === elements[i].element){
@@ -75,19 +85,63 @@ var ox = (
                     }
                 }
                 return false;
+            },
+            removeElement:function(el){
+                let callback = function(el){
+                    if(helper.isExistElement(el)){
+                        let index = helper.findElementIndex(el);
+                        elements.splice(index, 1);
+                    }
+                }
+                helper.scanHTML(el, callback);
+
             }
 
         };
         let listBindings = [
-            {binder:'o-text', fn:function(el, value){ el.innerText=value;}},
-            {binder:'o-click', fn:function(el, value){ el.addEventListener("click", value);}},
-            {binder:'o-for', fn:function(el, value){
+            {binder:'o-text', calc:true, fn:function(el, value){ el.innerText=value;}},
+            {binder:'o-click', calc:true, fn:function(el, value){
+                if(typeof value !== 'function'){
+                    throw new Error('o-click value must be an function!');  
+                }
+                let index = this.$index;
+                let that = this;
+                let callback = function(){   
+                    that.$index = index;            
+                    value.call(that);    
+                }
+                 el.addEventListener("click", callback);
+            }},
+            {binder:'o-value', calc:false, fn:function(el, value, index = null){
+                if(index !== null){
+                    return;
+                }
+                let that = this;
+                el.value = that[value];
+                let callback = function(){   
+                    that[value] = el.value;
+                }
+                 el.addEventListener("input", callback);
+            }},
+            {binder:'o-for', calc:true, fn:function(el, value, index = null){
                 if(!Array.isArray(value)){
                     throw new Error('Value must be an array!');
                 }
-                let localContext = {$base:virtualViewModel, $index:0, get $data(){return value[this.$index]}}
-                let virtualEl = el.cloneNode(true);
+
+                let localContext = {$base:virtualViewModel, $index:0, get $data(){return value[this.$index] }}
+                let virtualEl;
+                
+                if(index === null){
+                    virtualEl = el.cloneNode(true);
+                    elements[elements.length - 1].nativeElement = virtualEl;
+                }
+                else{
+                    helper.removeElement(el);
+                    virtualEl = elements[index].nativeElement;
+                }
+
                 el.innerHTML = "";
+
                 for(let i = 0 ; i < value.length; i++){
                     if(typeof value[i] === 'object'){
                         let keys = Object.keys(value[i]);
@@ -104,9 +158,10 @@ var ox = (
                 }
 
                 let arr = [{el:el, count:virtualEl.childElementCount}];
-                let fn =function(oneEl) {
+
+                let callback = function(oneEl) {
                     let newElement = oneEl.cloneNode(true);
-                    helper.scanBinder(newElement, localContext); 
+                    helper.scanBinderMX.call(localContext, newElement); 
 
                         if(newElement.childElementCount > 0){
                             let cloneEl = newElement.cloneNode(true);
@@ -131,11 +186,39 @@ var ox = (
 
                 for(let i = 0; i < value.length; i++){
                     localContext.$index = i;
-                    helper.scanHTML(virtualEl, fn, true);
+                    helper.scanHTML(virtualEl, callback, true);
                 }
-                throw new Error('BCI');
+                if(isInitMode){
+                    throw new Error('BCI');
+                }
             }},
-            {binder:'o-if', fn:function(el, value){
+            {binder:'o-attr', calc:true, fn:function(el,value){
+                if(typeof value !== 'object'){
+                    throw new Error('o-attr value must be an object!');   
+                }
+
+                let keys = Object.keys(value);
+
+                for (let i = 0; i < keys.length; i++)
+                {
+                    el[keys[i]] = value[keys[i]];
+                }
+
+            }},
+            {binder:'o-style', calc:true, fn:function(el,value){
+                if(typeof value !== 'object'){
+                    throw new Error('o-attr value must be an object!');   
+                }
+
+                let keys = Object.keys(value);
+
+                for (let i = 0; i < keys.length; i++)
+                {
+                    el.style[keys[i]] = value[keys[i]];
+                }
+
+            }},
+            {binder:'o-if', calc:true, fn:function(el, value){
                 let _el = helper.findElementDESC(el);
                 if(!value){
                     _el.saveInnerHTML = _el.element.innerHTML;
@@ -146,7 +229,7 @@ var ox = (
                     el.innerHTML = _el.saveInnerHTML;
                     helper.scanHTML(el, function(el){
                         if(!helper.isExistElement(el)){
-                            helper.scanBinder(el);
+                            helper.scanBinderMX(el);
                         }
                     });
                     _el.saveInnerHTML = null;
@@ -195,12 +278,15 @@ var ox = (
                                 Object.defineProperty(virtualViewModel, keys[i].toString(), {
                                     get: function() {
                                       return viewModel[keys[i]]()
+                                    },
+                                    set: function(value) {
+                                        viewModel[keys[i]](value);
                                     }
                                   });
                             }
                         }
                     }
-                    helper.scanHTML(root, function(root) {helper.scanBinder(root, virtualViewModel)});
+                    helper.scanHTML(root, function(root) {helper.scanBinderMX.call(virtualViewModel, root)});
                     isInitMode = false;
 
             },
@@ -215,7 +301,7 @@ var ox = (
                 let index;
                 let fn = function(value = null){ 
                     if(value === null){
-                       if (isInitMode) {indexes.push(index);}
+                        indexes.push(index);
                         return listUpdaters[index].value;
                     }
         
@@ -234,8 +320,9 @@ var ox = (
                                             if(elements[i].index !== null){
                                                 elements[i].context.$index = elements[i].index;
                                             }
+                                            let result;
                                             with(elements[i].context){result = eval("(" + elements[i].expression + ")")};
-                                            listBindings[b].fn(elements[i].element, result);  
+                                            listBindings[b].fn(elements[i].element, result, i);  
                                             break;
                                         }
                                     }
